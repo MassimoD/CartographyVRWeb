e59879298f8ef0f2802b9e973734cd7dff4a1075
Maxime Di Lullo <massimo.dil@gmail.com>
2018-02-11 11:40:16 -0500

    Data
    Added a new plane of content

--- js/data.js
+++ js/data.js
@@ -33,17 +33,28 @@ window.cartography.planes = [
 			'width' : '24',
 		}
 	},
-	{
-		'height' : '2',
-		'width' : '9',
-		'rotation' : '0 45 0',
-		'position' : '-30 0 -15',
-		'backgroundColor' : '#202020',
-		'text' : {
-			'content' : 'Knowledge',
-			'width' : '16',
-		}
-	},
+    {
+        'height' : '2',
+        'width' : '9',
+        'rotation' : '0 45 0',
+        'position' : '-30 0 -15',
+        'backgroundColor' : '#202020',
+        'text' : {
+            'content' : 'Epistemology',
+            'width' : '16',
+        }
+    },
+    {
+        'height' : '5',
+        'width' : '12',
+        'rotation' : '0 45 0',
+        'position' : '-30 -4 -15',
+        'backgroundColor' : '#202020',
+        'text' : {
+            'content' : "Julia Kristeva's account of dialectics  as catastrophe  in order to explain how creative practice engenders material processes that rupture meaning and language to allow the as yet unimagined and unrepresented to be revealed. (Barrett 2014, p. 54)",
+            'align' : 'left',
+        }
+    },
 	{
 		'height' : '2',
 		'width' : '9',
@@ -51,21 +62,20 @@ window.cartography.planes = [
 		'position' : '30 0 -15',
 		'backgroundColor' : '#202020',
 		'text' : {
-			'content' : 'Epistemology',
+			'content' : 'Knowledge',
 			'width' : '16',
 		}
 	},
-	{
-		'height' : '5',
-		'width' : '12',
-		'rotation' : '0 -45 0',
-		'position' : '30 -4 -15',
-		'backgroundColor' : '#202020',
-		'text' : {
-			'content' : "Julia Kristeva's account of dialectics  as catastrophe  in order to explain how creative practice engenders material processes that rupture meaning and language to allow the as yet unimagined and unrepresented to be revealed. (BarreW 2014, p. 54)",
-			'align' : 'left',
-		}
-	},
+    {
+        'height' : '5',
+        'width' : '12',
+        'rotation' : '0 -45 0',
+        'position' : '30 -4 -15',
+        'backgroundColor' : '#202020',
+        'text' : {
+            'content' : 'The juxtaposing of disparate objects and ideas has, after all,  often been viewed as an intrinsic aspect of creativity. The interplay of ideas from disparate areas of knowledge in creative arts research creates conditions for the emergence of new analogies, metaphors and models for understanding objects of enquiry.( Barrett 2007b, p.7)',
+        }
+    },
 
 	// Les données doivent être rajoutés juste au dessus de cette ligne de commentaire
 ];
